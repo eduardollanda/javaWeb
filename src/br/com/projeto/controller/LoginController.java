f8281cddb1d8d3510267509266dfa99dd476dbf6
eduardollanda <eduardohollanda14@gmail.com>
2019-11-18 23:27:19 -0300

    primeiro commit

---
+++ src/br/com/projeto/controller/LoginController.java
@@ -0,0 +1,55 @@
+package br.com.projeto.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+		String email = request.getParameter("email");
+		String senha = request.getParameter("password");
+		
+		if (email.equals("admin") && senha.equals("admin")) {
+			System.out.println("Usuario logado");
+			
+			response.sendRedirect("pages/index.html");
+		}else {
+			System.out.println("Login e/ou senha invalidos");
+			RequestDispatcher rd = request.getRequestDispatcher("index.html");
+			rd.forward(request, response);
+		}
+	}
+
+}
